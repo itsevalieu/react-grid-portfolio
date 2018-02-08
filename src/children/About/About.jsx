cc9f6ec1c99ba7b1bb238df5e2d62a31d94380c3
itsevalieu <itsevalieu@gmail.com>
2018-02-07 23:26:57 -0800

    Working on component rendering, adding onClick functions

---
+++ src/children/About/About.jsx
@@ -0,0 +1,25 @@
+import React, { Component } from "react";
+import "./About.css";
+
+class About extends Component {
+	render() {
+		return(
+			<div className="about">
+				<div className="about-image">
+					<img src='#' alt="Eva Lieu"/>
+				</div>
+				<div className="about-biography">
+					<p>Present: Eva Lieu is a web developer versed in data science and data visualization. She has worked previously worked in at a tech startup called SueYa as a front-end web developer and is currently working at a tea company called Bird Pick Tea & Herb as their General and Operations Manager.</p>
+					<p>Past: UCLA Alumni, English Major and Labor and Workplace Studies minor; Coding Bootcamp with UCLA Extension for Full Stack Web development</p>
+					<p>Interests: Data Visualization, Public Policy, Business Logic, Storytelling</p>
+				</div>
+				<div className="about-info">
+					<p>Skills: HTML3, CSS3, Javascript, Node.js, Express, React, MySQL, MongoDB, Python, APIs</p>
+					<p>Experience: Sueya, Bird Pick Tea & Herb, Teafographic</p>
+				</div>
+			</div>
+		);
+	}
+}
+
+export default About;
--- src/containers/App.js
+++ src/containers/App.js
@@ -1,7 +1,16 @@
 import React, { Component } from 'react';
 import './App.css';
-import hamburger from './hamburger.png'
+import hamburger from './hamburger.png';
+import About from '../children/About/About.jsx';
+import Portfolio from '../children/Portfolio/Portfolio.jsx';
+import Contact from '../children/Contact/Contact.jsx';
+
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.pushChildren = this.pushChildren.bind(this);
+    this.onClick = this.onClick.bind(this);
+  }
   render() {
     return (
       <div className="App">
@@ -10,16 +19,32 @@ class App extends Component {
           <button className="hamburger-icon"><img src={hamburger} alt="hamburger icon"/></button>
         </header>
         <aside>
-          <button>ABOUT</button>
-          <button>PORTFOLIO</button>
-          <button>CONTACT</button>
+          <button value="about" onClick={this.onClick}>ABOUT</button>
+          <button value="portfolio" onClick={this.onClick}>PORTFOLIO</button>
+          <button value="contact" onClick={this.onClick}>CONTACT</button>
         </aside>
-        <main>
-          MAIN
-        </main>
+        <main id="children"></main>
         <footer><p>© 2018 EVA LIEU</p></footer>
       </div>
     );
+  }
+  onClick(e) {
+    var childValue = e.target.value;
+    console.log(childValue);
+    var insertHere = document.getElementById("children");
+    
+    var something = this.pushChildren(childValue);
+    insertHere.push(<About/>);
+    console.log(something);
+  }
+  pushChildren(value) {
+    if(value === "about") {
+      return '<About/>';
+    } else if (value === "portfolio") {
+      return '<Portfolio/>';
+    } else {
+      return '<Contact/>';
+    }
   }
 }
 
