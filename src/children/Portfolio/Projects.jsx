ca99647e69cc584cd77022474da60b0012eb8628
itsevalieu <itsevalieu@gmail.com>
2018-02-27 23:56:57 -0800

    Adding projects to project component

--- src/children/Portfolio/Projects.jsx
+++ src/children/Portfolio/Projects.jsx
@@ -2,6 +2,36 @@ import React, { Component } from "react";
 import "./Projects.css";
 
 class Project extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      projects: [
+        { path: '/twitterbot',
+          exact: true, 
+          className: 'button-description',
+          name: 'Twitter Bot',
+          description: "Twitter bot live streaming and sentiment analysis.",
+          main: () => <Project/>
+        },
+        { path: '/drinkgenerator',
+          className: 'button-description',
+          name: 'Random Drink Generator',
+          description: "Randomly picks a drink off a menu for you.",
+          main: () => <Project/>
+        },
+        { path: '/nytreact',
+          className: 'button-description',
+          name: 'NYT API',
+          description: "Search New York Times for articles.",
+          main: () => <Project/>
+        },
+        { path: '/teafographic',
+          className: 'button-description',
+          name: 'Teafographic',
+          description: "Website for tea fanatics.",
+          main: () => <Project/>
+        }
+      ],
   render() {
     return(
       <div className="project">
